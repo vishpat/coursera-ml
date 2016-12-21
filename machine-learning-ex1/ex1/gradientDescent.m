2c10e2eb5c33cd75def2bfa5ad2f2e6307003d73
vpati011 <vishal.patil@nuagenetworks.net>
2016-12-20 23:04:29 -0500

    Completed single variable assignment

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    
+    err_diff = X*theta - y;
+    theta_0 = theta(1) - (alpha * sum(err_diff))/m; 
+    theta_1 = theta(2) - (alpha * sum(err_diff .* X(:,2)))/m;
+    
+    theta = [theta_0 theta_1]';
 
     % ============================================================
 
