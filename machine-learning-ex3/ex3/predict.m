66b1e693f3f7ebc5af19978749bc4d379545261d
vpati011 <vishal.patil@nuagenetworks.net>
2016-12-28 11:31:23 -0500

    Finished neural net assignment

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -9,6 +9,7 @@ num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
+X = [ones(m, 1) X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -21,12 +22,11 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a2 = sigmoid(X*Theta1');
+a2 = [ones(m, 1) a2];
 
-
-
-
-
-
+a3 = sigmoid(a2*Theta2');
+[q, p] = max(a3, [], 2);
 
 
 % =========================================================================
