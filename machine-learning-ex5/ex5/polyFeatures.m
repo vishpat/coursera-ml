cd0f48ed2ce3f775cff997dfcbac61dae904e8f9
vpati011 <vishal.patil@nuagenetworks.net>
2017-01-06 22:31:53 -0500

    Implemented poly features

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -7,7 +7,7 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+% X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
@@ -15,10 +15,10 @@ X_poly = zeros(numel(X), p);
 %
 % 
 
-
-
-
-
+X_poly = X;
+for i=2:p
+    X_poly = [X_poly X.^i];
+end
 
 % =========================================================================
 
