f1031261eaba06dca505df9e6d229ac66fe3a01c
vpati011 <vishal.patil@nuagenetworks.net>
2017-01-08 20:20:09 -0500

    Minimized the model error

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 1.0;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,37 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+%C_trials = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%sigma_trials = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%
+%
+%C_idx = 1;
+%C_best_idx = 1;
+%
+%sigma_idx = 1;
+%sigma_best_idx = 1;
+%
+%min_err = 100000;
+%
+%for C_idx = 1:size(C_trials, 2)
+%    for sigma_idx = 1:size(sigma_trials, 2)
+%        C_current = C_trials(C_idx);
+%        sigma_current = sigma_trials(sigma_idx);
+%        
+%        model= svmTrain(X, y, C_current, @(x1, x2) gaussianKernel(x1, x2, sigma_current));
+%        predictions  = svmPredict(model, Xval);
+%        curr_err = mean(double(predictions ~= yval));
+%        
+%        if curr_err < min_err
+%            min_err = curr_err;
+%            C_best_idx = C_idx;
+%            sigma_best_idx = sigma_idx;
+%        end
+%    end
+%end
+%
+%C = C_trials(C_best_idx);
+%sigma = sigma_trials(sigma_best_idx);
 
 % =========================================================================
 
